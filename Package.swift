05726f295a8d248d99f9d6d2ff9b4d13fa941899
Olaf Neumann <github@olafneumann.org>
2023-08-01 14:33:49 +0200

    Add exit button

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ import PackageDescription
 let package = Package(
     name: "NoxoneUtils",
     platforms: [
-        .iOS(.v14), .tvOS(.v13), .watchOS(.v7)
+        .iOS(.v15), .tvOS(.v13), .watchOS(.v7)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
